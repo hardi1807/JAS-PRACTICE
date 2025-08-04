8450f3635beb3cc6cfc88705b3c3d213c11c4063
hardi1807 <sathvara07@gmail.com>
2025-08-04 07:01:36 +0000

    data

---
+++ 01-basics/03-conversion.js
@@ -0,0 +1,35 @@
+let score="0"
+console.log( typeof score);
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+Converting string to number
+
+"33" => 33
+"33abc" => NaN (Not a Number)
+true => 1
+false => 0
+null => 0
+
+*/
+
+let isloggedIn = 1;
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log( booleanIsLoggedIn);
+ 
+/*
+Converting number to boolean
+
+1 => true
+0 => false
+"abc" => true
+"" => false
+null => false
+undefined => false
+NaN => false
+*/
+
