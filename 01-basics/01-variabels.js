a60ed967bef01eace0d914cd97f037a6e5b7c94b
hardi1807 <sathvara07@gmail.com>
2025-08-04 06:30:28 +0000

    data

---
+++ 01-basics/01-variabels.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "hardi@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState = "Rajasthan"
+
+accountEmail="hc@gmail.com"
+accountPassword="123"
+accountCity = "delhi"
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState]);
+
+// not using var becuase it is function scoped and having issue with block scope
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version
+
+// alert("3+3")//we are using node.js ,not browser
+
+
+console.log(3 + 3)// Output: 6
+console.log("hardi")/*code readability should be high  */
+
+/*  Data Types in JavaScript:
+(primitive types)
+number => 2 to power 52 
+bigint
+string "text"
+boolean true/false"
+null = standlone value
+undefined = not assigned value
+symbol = unique and immutable value
+*/
+
+console.log(typeof null); 
+// Output: object (this is a known bug in JavaScript)
+console.log(typeof undefined);
+// Output: undefined
