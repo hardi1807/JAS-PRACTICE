91bd0f4c4790023d56b4ab0bd8deb9b7d2ed2e89
hardi1807 <sathvara07@gmail.com>
2025-08-05 06:00:36 +0000

    data

---
+++ 01-basics/04-comperison.js
@@ -0,0 +1,12 @@
+// console.log("2" > 1);
+
+console.log(null > 0); // false
+console.log(null == 0);//false
+console.log(null >= 0);// true
+
+// same as undefined but in undefined all the comparisons are false
+
+console.log("2"==2); // true
+console.log("2"===2); // false because type is different
+
+
