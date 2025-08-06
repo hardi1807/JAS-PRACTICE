bf61d4044c113bcd6ffcfd3d928327fe160b8a0f
hardi1807 <sathvara07@gmail.com>
2025-08-06 04:47:36 +0000

    data

---
+++ 01-basics/06-math.js
@@ -0,0 +1,20 @@
+const score=400
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+
+const otherNumber=  20.8554
+console.log(otherNumber.toPrecision(3));
+  
+
+//                          math  
+
+/*
+math.abs = for absolute
+math.round
+math.ceil
+min
+max
+floor
+math.random between 0 and 1
