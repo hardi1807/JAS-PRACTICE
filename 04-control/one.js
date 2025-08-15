fbd34ff685f2b9add33d949f529f58c6dfd59e6a
hardi1807 <sathvara07@gmail.com>
2025-08-15 14:18:09 +0000

    data

---
+++ 04-control/one.js
@@ -0,0 +1,23 @@
+/*
+ if statement    
+
+ if (condition) {
+     // code to be executed if condition is true
+ } else {
+     // code to be executed if condition is false
+ }
+     for type checking use === 
+     */
+
+const temp = 41
+
+if (temp === 41) {
+    console.log("Temperature is 41 degrees");
+} else {
+    console.log("Temperature is not 41 degrees");
+}
+
+console.log("execute");
+
+
+
