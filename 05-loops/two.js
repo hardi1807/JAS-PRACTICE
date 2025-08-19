86a5720550db1e5b98d75224b87df620104cf42e
hardi1807 <sathvara07@gmail.com>
2025-08-19 19:49:24 +0000

    data

---
+++ 05-loops/two.js
@@ -0,0 +1,27 @@
+/*  while loop
+
+let index=0
+while (condition) {
+    console.log()
+    index = index+1
+}
+    */
+
+
+let i=0
+
+while( i<=10){
+   console.log(i);
+   i= i+2
+}
+
+let myArray = ['hi', 'hello','naaste']
+
+let arr=0
+ while (arr<myArray.lenght){
+    console.log(`value is ${myArray[arr]}`);
+    arr=arr+1
+ }
+
+
+
